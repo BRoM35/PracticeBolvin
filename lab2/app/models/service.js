0b4b343252c3e6d7102a254323659f00fb42a0af
r.v.bolvin <r.v.bolvin@tmn3.etagi.com>
2020-04-07 18:39:22 +0500

    create lab2

---
+++ lab2/app/models/service.js
@@ -0,0 +1,13 @@
+module.exports = (mongoose) => {
+  const Schema = mongoose.Schema;
+  const serviceSchema = new Schema({
+    name: {
+      type: String,
+      required: true},
+    price: {
+      type: Number,
+      required: true}
+  });
+
+  return mongoose.model('Service', serviceSchema);
+};
